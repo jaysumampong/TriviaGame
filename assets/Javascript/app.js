0180e362889d9ba94aeeb36cc5c13f16a7681353
Meng Hao <jaysumampong@gmail.com>
2019-06-12 21:48:58 -0600

    finished html

---
+++ assets/Javascript/app.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+// Set Event Listeners
+$("#body").hide();
+$("#win-screen").hide();
+$("#start").on('click', startGame);
+// Set up Correct, Uncorrect and Unanswered counters
+    let right = 0;
+    let wrong= 0;
+    let unanswered = 0;
+    
+    // Set up Function for timer interval
+    let number = 3;
+    let intervalId;
+
+    function startGame() {
+        $("#body").show();
+        $("#start").hide();
+        timerStart();
+        
+    };
+
+    function timerStart() { 
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    };
+    
+    function decrement() {
+        number--;
+        $("#time-remaining").html(number);
+        if (number === 0) {
+            winScreen();
+        }
+    };
+
+    function winScreen() {
+        $("#body").hide();
+        $("#win-screen").show();
+    };
+    // set up functions to determine if answers are right or wrong
+    function correctAnswer(){
+        right++;
+    };
+
+    function incorrectAnswer(){
+        wrong++;
+    };
+
+    function notanswered(){
+        unanswered++;
+    };
+
+    // Trivia questions
+    let triviaQuestions = {
+
+    }
+
+
+
+
+
+
+
+
+})
